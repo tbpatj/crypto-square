8d1ba87cedb39f50ca1b11f3756c46651706763a
tbpatj <luebketj@gmail.com>
2022-03-19 08:38:51 -0600

    fixed registration endpoints and front end

---
+++ frontend/src/pages/LinkDevice.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useState } from "react";
+function isNumeric(val) {
+  return /^-?\d+$/.test(val);
+}
+
+export default function LinkDevicePage() {
+  const [digit, setDigit] = useState("");
+
+  function updateDigit(e) {
+    if (e.target.value === "") {
+      setDigit(e.target.value);
+    }
+    if (e.target.value.length <= 4 && isNumeric(e.target.value)) {
+      setDigit(e.target.value);
+    }
+  }
+  function submitCode(e) {
+    e.preventDefault();
+    if (digit.length === 4) {
+    }
+  }
+  return (
+    <div className="container text-center">
+      <h1>Link Device</h1>
+      <form onSubmit={(e) => submitCode(e)}>
+        <input
+          value={digit}
+          onChange={(e) => updateDigit(e)}
+          style={{ width: "200px", fontSize: "70px", textAlign: "center" }}
+        ></input>
+      </form>
+    </div>
+  );
+}
